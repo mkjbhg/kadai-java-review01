46c372c1dbc1090fc64ce580cf2317a135f7ae65
mkjbhg <piano@192.168.1.15>
2024-12-16 13:11:53 +0900

    Javaプログラミング入門学習終了

---
+++ src/ArithmeticExceptionSample.java
@@ -0,0 +1,13 @@
+
+public class ArithmeticExceptionSample {
+
+    public static void main(String[] args) {
+        add(20,30);
+        add(10,50,40);
+        add(10.2,6.8);
+
+}
+    
+    public static void add(int num1,int num2);
+      System.out.println("int型2つの加算結果："　+ (num1 + num2));
+
---
+++ src/ArithmeticOperations.java
@@ -0,0 +1,15 @@
+
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 10;
+
+        int result1 = num1 + num2;
+
+        
+        
+        System.out.println("num1 + num2 = " + result1);
+    }
+
+}
