8df05ccd2031e6274b48e5e376d2d8fac8ed456f
mkjbhg <piano@192.168.1.15>
2024-12-18 16:25:13 +0900

    レッスン4-6

---
+++ src/IfSample1.java
@@ -0,0 +1,14 @@
+
+public class IfSample1 {
+
+    public static void main(String[] args) {
+       int score = -10;
+       if (score >= 60){
+     System.out.println("合格です！");
+       }else {
+           System.out.println("赤点です...");
+       }
+
+    }
+
+}
---
+++ src/Triangle.java
@@ -0,0 +1,19 @@
+
+public class Triangle {
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 4;
+        int c = 5;
+
+        if(a == b && b == c && c==a) {
+           System.out.println("正三角形");
+        }else if(a==b || b==c || c==a) {
+            System.out.println("二等辺三角形");
+        }else {
+            System.out.println("不当辺三角形");
+
+        }
+    }
+
+}
